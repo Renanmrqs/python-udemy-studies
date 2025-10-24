b92362d4b56c66627735895bb355478009a70be4
Renanmrqs <renanmarques1923@gmail.com>
2025-10-24 10:48:07 -0300

    Refatora: Move todos os arquivos para a pasta 'modulo-1' para melhor organização do repositório.

---
+++ Modulo 1 - Lógica de programação basica/operadoresaritmeticos.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('Adição:', adicao)
+
+subtracao = 10 - 5
+print('Subtracao:', subtracao)
+
+multiplicacao = 5 * 5
+print('multiplicacao:', multiplicacao)
+
+divisao = 10 / 3
+print('Divisao:', divisao)
+
+divisao_inteira = 10 // 3
+print('divisao intera:', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciacao:', exponenciacao)
+
+modulo = 55 % 2 # resto da divisao
+print('modulo:', modulo)
+
+print(10 % 8 == 0)
+print('é par?:', 17 % 2 == 0)
