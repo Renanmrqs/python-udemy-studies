890c854cdaea354a8a44ca92cc3285a6b55889b3
Renanmrqs <renanmarques1923@gmail.com>
2025-10-25 15:22:19 -0300

    k

--- Modulo 1 - Lógica de programação basica/metodo format.py
+++ Modulo 1 - Lógica de programação basica/metodo format.py
@@ -1,6 +1,8 @@
-a = 'A'
-b = 'B'
-c = 1.1
-formato = ''
+a = 'Aaaaaa'
+b = 'Bola'
+c = 377.1678
+string = 'a={nome1} b={nome2} c={nome3:.0f}'
+formato = string.format(nome1=a, nome2=b, nome3=c)
 
 print(formato)
+print('ai git')
