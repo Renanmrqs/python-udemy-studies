2c90902418236a051f8d60b2b54870a7d8866a8c
Renanmrqs <renanmarques1923@gmail.com>
2025-10-27 17:55:41 -0300

    news(1)

---
+++ Modulo 1 - Lógica de programação basica/introd if/elif/else.py
@@ -0,0 +1,22 @@
+# if / elif       / else
+# se / se nao se  / se nao
+#entrada = input('voce que "entrar" ou "sai"?')
+
+
+# if entrada == 'entrar':
+#     print('Voce entrou no sistema')
+# elif entrada == 'sair':
+#     print('Voce saiu do sistema')
+# else:
+#     print('voce nao digitou nem entrar nem sair.')
+
+condicao = False
+
+if condicao:
+    print('este e o codigo do if')
+else:
+    print('')
+
+print('Fora do bloco')
+
+
