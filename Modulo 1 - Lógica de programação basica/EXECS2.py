ca46c5a8f07f13dbfccbaa8d5d3f86d5ae4f7a67
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2025-10-28 13:35:05 -0300

    news(4)

---
+++ Modulo 1 - Lógica de programação basica/EXECS2.py
@@ -0,0 +1,26 @@
+# (MEU CODIGO ABAIXO)
+# primeiro_valor = input('Digite um valor: ')
+# segundo_valor = input('Digite outro valor: ')
+
+# primeiro_valor_int = int(primeiro_valor)
+# segundo_valor_int= int(segundo_valor)
+
+# if  primeiro_valor_int > segundo_valor_int:
+#     print (f'o {primeiro_valor =} é maior que o {segundo_valor =}')
+# else primeiro_valor_int < segundo_valor_int:
+#     print(f'o {primeiro_valor =} é menor que o {segundo_valor =}')
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if  primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual'
+        f' ao que {segundo_valor=}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior'
+        f' do que {primeiro_valor=}'
+    )
+    
